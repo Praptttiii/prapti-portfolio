a345d4d9d482e1016a263d45c17e4f1cf039359e
Prapti <praptipatel12unjha@gmail.com>
2026-04-03 17:30:30 +0530

    changed congif file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/prapti-portfolio/",
   plugins: [react()],
   resolve: {
     alias: {
